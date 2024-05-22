07fbe3a7cff70b44fde9d0431b6c2c6bb51b4a73
JD17VM <jdiegodiego4@gmail.com>
2024-05-21 23:30:03 -0500

    Se elimina la funcion que inicializaba todo y nos quedamos con el constructor

--- CONTROL_HDD/crear_disco.cpp
+++ CONTROL_HDD/crear_disco.cpp
@@ -160,65 +160,6 @@ struct Estructura_HDD
         }
     }
 
-    void llenarEstructuraHDD(string nombre_disco, string nombre_platos, int cant_platos_por_disco, char caras_a, char caras_b, string nombre_pistas, int cant_pistas_por_cara, string nombre_bloques, int cant_bloques_por_pista)
-    {
-        ESPACIO_HDD.tipo = "HDD";
-        ESPACIO_HDD.direccion = "HDD";
-        ESPACIO_HDD.capacidad_total = 0;
-        ESPACIO_HDD.capacidad_usada = 0;
-
-        for (int i = 1; i <= cant_platos_por_disco; i++)
-        {
-            const string plato_a = nombre_disco + "/" + nombre_platos + "_" + completarNumeroConCeros(i, 2) + "_" + caras_a;
-            const string plato_b = nombre_disco + "/" + nombre_platos + "_" + completarNumeroConCeros(i, 2) + "_" + caras_b;
-            Espacio_HDD PLATO_A;
-            Espacio_HDD PLATO_B;
-            PLATO_A.tipo = "PLATO_CARA";
-            PLATO_B.tipo = "PLATO_CARA";
-            PLATO_A.direccion = plato_a;
-            PLATO_B.direccion = plato_b;
-            PLATO_A.capacidad_total = 0;
-            PLATO_B.capacidad_total = 0;
-            PLATO_A.capacidad_usada = 0;
-            PLATO_B.capacidad_usada = 0;
-            for (int j = 1; j <= cant_pistas_por_cara; j++)
-            {
-                const string pista_a = plato_a + "/" + nombre_pistas + "_" + completarNumeroConCeros(j, 2);
-                const string pista_b = plato_b + "/" + nombre_pistas + "_" + completarNumeroConCeros(j, 2);
-                Espacio_HDD PISTA_A;
-                Espacio_HDD PISTA_B;
-                PISTA_A.tipo = "PISTA";
-                PISTA_B.tipo = "PISTA";
-                PISTA_A.direccion = pista_a;
-                PISTA_B.direccion = pista_b;
-                PISTA_A.capacidad_total = 0;
-                PISTA_B.capacidad_total = 0;
-                PISTA_A.capacidad_usada = 0;
-                PISTA_B.capacidad_usada = 0;
-                for (int k = 1; k <= cant_bloques_por_pista; k++)
-                {
-                    const string bloque_a = pista_a + "/" + nombre_bloques + "_" + completarNumeroConCeros(k, 2) + ".txt";
-                    const string bloque_b = pista_b + "/" + nombre_bloques + "_" + completarNumeroConCeros(k, 2) + ".txt";
-                    Espacio_HDD BLOQUE_A;
-                    Espacio_HDD BLOQUE_B;
-                    BLOQUE_A.tipo = "BLOQUE";
-                    BLOQUE_B.tipo = "BLOQUE";
-                    BLOQUE_A.direccion = bloque_a;
-                    BLOQUE_B.direccion = bloque_b;
-                    BLOQUE_A.capacidad_total = 5;
-                    BLOQUE_B.capacidad_total = 5;
-                    BLOQUE_A.capacidad_usada = 5;
-                    BLOQUE_B.capacidad_usada = 5;
-                    PISTA_A.vector_espacios_hdd.push_back(BLOQUE_A);
-                    PISTA_B.vector_espacios_hdd.push_back(BLOQUE_B);
-                }
-                PLATO_A.vector_espacios_hdd.push_back(PISTA_A);
-                PLATO_B.vector_espacios_hdd.push_back(PISTA_B);
-            }
-            ESPACIO_HDD.vector_espacios_hdd.push_back(PLATO_A);
-            ESPACIO_HDD.vector_espacios_hdd.push_back(PLATO_B);
-        }
-    }
 
     void crearCarpetasArchivos()
     {
