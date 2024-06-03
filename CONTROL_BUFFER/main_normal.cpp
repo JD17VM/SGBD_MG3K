d085b5ba617de560d5db935b02c657cb8e74bbaf
JD17VM <jdiegodiego4@gmail.com>
2024-06-03 11:40:41 -0500

    .

--- CONTROL_BUFFER/main_normal.cpp
+++ CONTROL_BUFFER/main_normal.cpp
@@ -12,18 +12,6 @@
 
 using namespace std;
 
-void mostrarMenu() {
-    cout << "========= MENÚ =========" << endl;
-    cout << "1. Bloque_1" << endl;
-    cout << "2. Bloque_2" << endl;
-    cout << "3. Bloque_3" << endl;
-    cout << "4. Bloque_4" << endl;
-    cout << "5. Bloque_5" << endl;
-    cout << "6. Bloque_6" << endl;
-    cout << "0. Salir" << endl;
-    cout << "========================" << endl;
-}
-
 
 int main(){
 
@@ -46,49 +34,6 @@ int main(){
     p.imprimirEstado();
     p.llamarABloque("Bloques/BLOQUE_02.txt");
     p.imprimirEstado();*/
-
-    int opcion = 0;
-    char opcion2 = 0;
-    bool salir = false;
-    system("cls");
-    p.imprimirEstado();
-    while(!salir){
-
-        mostrarMenu();
-        cout << "Lectura o escritura: ";
-        cin >> opcion2;
-        cout << "Selecciona una opción: ";
-        cin >> opcion;
-
-
-        switch(opcion){
-            case 1:
-                p.llamarABloque("Bloques/BLOQUE_01.txt");
-                break;
-            case 2:
-                p.llamarABloque("Bloques/BLOQUE_02.txt");
-                break;
-            case 3:
-                p.llamarABloque("Bloques/BLOQUE_03.txt");
-                break;
-            case 4:
-                p.llamarABloque("Bloques/BLOQUE_04.txt");
-                break;
-            case 5:
-                p.llamarABloque("Bloques/BLOQUE_05.txt");
-                break;
-            case 6:
-                p.llamarABloque("Bloques/BLOQUE_06.txt");
-                break;
-            case 0:
-                salir = true;
-                break;
-        }
-        system("cls");
-        p.imprimirEstado();
-        
-    }
-
 
     return 0;
 }
